849eb0666984325a742320db488698359b81c60a
Kazuo Abduch <76076430+kazuo-abduch@users.noreply.github.com>
2022-08-31 16:39:36 -0300

    fix posts models

---
+++ backend/models/posts.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+  const Posts = sequelize.define('Posts', {
+    id: { type: DataTypes.INTEGER, primaryLey: true, autoIncrement: true },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    createDate: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  }, {
+    tableName: 'Posts',
+    timestamps: false,
+  });
+
+  Posts.associate = (models) => {
+    Posts.hasMany(models.Updates, {
+      foreignKey: 'postId',
+      as: 'updates',
+    })
+  }
+
+  return Posts
+}
