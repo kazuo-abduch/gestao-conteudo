9610284aa0b404d6ae8eb4d2f60b553c73f07012
Kazuo Abduch <76076430+kazuo-abduch@users.noreply.github.com>
2022-09-02 12:17:29 -0300

    fix api call

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,9 +1,10 @@
-const api_url = `http://localhost:3000`
+import axios from 'axios';
+const api_url = `http://localhost:3001`
 
 const getAllPosts = async () => {
-  const postResponse = await fetch(api_url);
-  const responseJson = await postResponse;
-  return postResponse.ok ? Promise.resolve(responseJson) : Promise.reject(responseJson);
+  const postResponse = await axios.get(api_url);
+  const responseJson = await postResponse.data;
+  return responseJson
 };
 
 export default getAllPosts;
